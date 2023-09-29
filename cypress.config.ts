0829922499704dd22f3dbe1f4cc5c132e430a3bf
Chmalo <chmalo.f@gmail.com>
2023-09-29 15:33:20 -0400

    instalacion y config de cypress

---
+++ cypress.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  video: false,
+  component: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    }
+  },
+  e2e: {
+    baseUrl: "http://127.0.0.1:5173/",
+    specPattern: "cypress/e2e/tests/*.cy.{js,jsx,ts,tsx}",
+    screenshotOnRunFailure: false,
+    video: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    supportFile: "cypress/e2e/support/e2e.ts",
+  },
+});
---
+++ cypress/e2e/tests/login.cy.ts
@@ -0,0 +1,9 @@
+
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('/')
+
+    // cy.get('input[name="email"]').should('be.visible');
+  })
+})
